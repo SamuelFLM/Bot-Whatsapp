d3b027046d083394417e26048ab9fef19817d692
SamuelFLM <felipelimaaa23@gmail.com>
2023-01-27 14:26:14 -0300

    Att Estrutura

---
+++ Src/Config_home.py
@@ -0,0 +1,38 @@
+
+from Page.Home import Interface_home
+import PySimpleGUI as sg
+import webbrowser
+janela_home = Interface_home()
+
+class Config_home:
+    
+    def __init__(self) -> None:
+        self.window = janela_home.page()
+        
+    def executar(self):
+        
+        while True:
+            event,values = self.window.read(timeout=1)
+            
+            if event == sg.WIN_CLOSED:
+                break
+            
+            
+            if event == "direita":
+                self.window["imagem"].update(filename="Src//Page//Img//Icon//Home//part3.png")
+                self.window["texto"].update(filename="Src//Page//Img//Icon//Home//texto3.png")
+                self.window["esquerda"].update(visible=True)
+                self.window["direita"].update(visible=False)
+                
+            if event == "esquerda":
+                self.window["imagem"].update(filename="Src//Page//Img//Icon//Home//part1.png")
+                self.window["texto"].update(filename="Src//Page//Img//Icon//Home//texto.png")
+                self.window["direita"].update(visible=True)
+                self.window["esquerda"].update(visible=False)
+            
+            if event == "como_utilizar":
+                webbrowser.open_new_tab("https://github.com/SamuelFLM/Bot-Whatsapp")    
+
+if __name__ == "__main__":
+    t = Config_home()
+    t.executar()
--- Src/Page/Home.py
+++ Src/Page/Home.py
@@ -3,7 +3,23 @@ import PySimpleGUI as sg
 class Interface_home:
     
     def __init__(self) -> None:
-        pass
+        self.background = "#0A1B1E"
     
-    def page():
-        pass
+    def page(self):
+        
+        cabecalho = [[sg.Image(filename="Src//Page//Img//Logo//logo.png", background_color=self.background, pad=(100,(20,20)))],
+                     [sg.Image(filename="Src//Page//Img//Icon//Home//texto.png", background_color=self.background, pad=(0,(0,30)),k="texto", visible=True)], 
+                    ]
+        carrocel = [
+            [sg.Image(filename="Src//Page//Img//Icon//esquerda_branca.png", background_color=self.background, pad=(10,(0,0)),k="esquerda", enable_events=True, visible=False),
+             sg.Image(filename="Src//Page//Img//Icon//Home//part1.png", background_color=self.background, pad=(20,(10,50)),k="imagem", visible=True),
+             sg.Image(filename="Src//Page//Img//Icon//direita_branca.png", background_color=self.background, pad=(10,(0,0)),k="direita", enable_events=True, visible=True),
+            ],
+        ]
+        rodape = [[sg.Image(filename="Src//Page//Img//Icon//Home//rodape1.png", background_color=self.background, pad=(0,(50,0)),k="entrar", enable_events=True)],
+                  [sg.Image(filename="Src//Page//Img//Icon//Home//rodape2.png", background_color=self.background, pad=(0,(0,0)),k="como_utilizar", enable_events=True)],
+                 ]
+        
+        layout = [cabecalho,carrocel,rodape]
+        window = sg.Window("Bot Whatsapp", layout=layout,size=(374, 718), grab_anywhere=True, margins=(0,0),element_justification='c', background_color=self.background,icon="Src//Page//Img//Logo//icon.ico")
+        return window
